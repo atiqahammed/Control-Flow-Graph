4b6d5a488ef5c544b3dc818db7aba37c2bc33e9a
atiqahammed <atiqahammedshamim@gmail.com>
2018-09-02 20:04:12 +0600

    mathods firs line now can be found

--- Codes/code1.java
+++ Codes/code1.java
@@ -12,4 +12,15 @@ class ForLoopExample {
               System.out.println("The value of i is: "+i);
          }
     }
+    
+    public static List<String> get_matches(String s, String p) 
+    {
+        // returns all matches of p in s for first group in regular expression 
+        List<String> matches = new ArrayList<String>();
+        Matcher m = Pattern.compile(p).matcher(s);
+        while(m.find()) {
+            matches.add(m.group(1));
+        }
+        return matches;
+    }
 }
--- src/class_analyzer/MethodFinder.java
+++ src/class_analyzer/MethodFinder.java
@@ -1,8 +1,10 @@
 package class_analyzer;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 import com.sun.org.apache.regexp.internal.recompile;
+import com.sun.org.apache.xerces.internal.impl.xs.identity.Selector.Matcher;
 
 public class MethodFinder {
 
@@ -15,8 +17,12 @@ public class MethodFinder {
 			if (i < allCodeLines.size() - 1) {
 				nextLine = allCodeLines.get(i + 1);
 			}
-			
-			if(isMethodStartingLine(currentLine, nextLine)) {
+
+			if (isMethodStartingLine(currentLine, nextLine)) {
+				
+				
+				System.out.println(currentLine);
+				
 				
 			}
 
@@ -27,18 +33,66 @@ public class MethodFinder {
 	public boolean isMethodStartingLine(String currentLine, String nextLine) {
 		String allWards[] = currentLine.split(" ");
 		boolean isClassDeclaration = checkClassDecleration(allWards);
+		boolean numverOfWordCheck = checkNumberOfWords(allWards);
+		boolean bracketFound = checkBreacket(currentLine);
+		boolean endWithSemicolon  = checkSemicolon(currentLine);
+		boolean keyWordFound = checkKeyWords(currentLine); 
+		boolean paranthesisFound = false;
 		
-		//if(isClassDeclaration)
-		//	System.out.println("class found");
+		if(nextLine == null) {
+			if(currentLine.endsWith("{")) paranthesisFound = true;
+		} else {
+			if(currentLine.endsWith("{")) paranthesisFound = true;
+			Pattern MY_PATTERN = Pattern.compile("\\{");
+			java.util.regex.Matcher matcher = MY_PATTERN.matcher(nextLine);
+			if(matcher.find()) paranthesisFound = true;
+			
+			//if(!paranthesisFound && nextLine.startsWith("{")) paranthesisFound = true; 
+		}
 		
-		
-		
+		if(!isClassDeclaration && !keyWordFound && numverOfWordCheck && bracketFound && !endWithSemicolon && paranthesisFound) return true;
+
+		return false;
+	}
+
+	private boolean checkKeyWords(String currentLine) {
+		Pattern MY_PATTERN = Pattern.compile("for");
+		java.util.regex.Matcher matcher = MY_PATTERN.matcher(currentLine);
+		if(matcher.find()) return true;
+		MY_PATTERN = Pattern.compile("while");
+		matcher = MY_PATTERN.matcher(currentLine);
+		if(matcher.find()) return true;
+		return false;
+	}
+
+	private boolean checkSemicolon(String currentLine) {
+		if(currentLine.endsWith(";")) return true;
+		return false;
+	}
+
+	private boolean checkBreacket(String currentLine) {
+		boolean startingBracker = false, endingBracket = false;
+		for (int i = 0; i < currentLine.length(); i++) {
+			if (currentLine.charAt(i) == '(')
+				startingBracker = true;
+			if (currentLine.charAt(i) == ')')
+				endingBracket = true;
+		}
+
+		if (startingBracker && endingBracket)
+			return true;
+		return false;
+	}
+
+	private boolean checkNumberOfWords(String[] allWards) {
+		if (allWards.length >= 2)
+			return true;
 		return false;
 	}
 
 	private boolean checkClassDecleration(String[] allWards) {
-		for(int i = 0; i < allWards.length; i++) {
-			if(allWards[i].equals("class"))
+		for (int i = 0; i < allWards.length; i++) {
+			if (allWards[i].equals("class"))
 				return true;
 		}
 		return false;
